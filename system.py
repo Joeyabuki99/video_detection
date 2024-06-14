440afecb608395bd1a1e29d3123d0a51bcae5bb4
Joeyabuki99 <161338073+Joeyabuki99@users.noreply.github.com>
2024-06-14 17:39:38 +0200

    system.py

--- system.py
+++ system.py
@@ -9,15 +9,12 @@ classifier_class_names = {
 }
 
 # Carica il modello YOLO
-detector = YOLO(r"C:\Users\fasul\Desktop\Sabato_UNI\TESI\code\runs\yolov8s_trained\weights\best.pt")
+detector = YOLO(r"C:\path\best.pt")
 
-# Carica il classificatore Keras
-classifier = load_model(r"C:\Users\fasul\Desktop\Sabato_UNI\TESI\code\saved_models\efficientnet_model_unfreeze_128.h5")
-
-video_path = r"C:\Users\fasul\Desktop\Sabato_UNI\TESI\code\video_test.mp4"
+video_path = r"C:\path\video_test.mp4"
 cap = cv2.VideoCapture(video_path)
 
-output_path = r"C:\Users\fasul\Desktop\Sabato_UNI\TESI\code\video_out.mp4"
+output_path = r"C:\path\video_out.mp4"
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 fps = cap.get(cv2.CAP_PROP_FPS)
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -36,25 +33,13 @@ while cap.isOpened():
     for box in detections:
         x1, y1, x2, y2 = box.xyxy[0].tolist()
         conf = box.conf[0].item()
-        cls = results[0].names[box.cls[0].item()]   #prendo il nome della classe per yolo
-        #cls = box.cls[0].item()
+        cls = results[0].names[box.cls[0].item()]   
 
         # Estrai la regione di interesse (ROI)
         roi = frame[int(y1):int(y2), int(x1):int(x2)]
 
-        # Preprocessa la ROI per il classificatore
-        roi_resized = cv2.resize(roi, (300, 300))  # Usa la dimensione richiesta dal tuo classificatore
-        roi_resized = roi_resized / 255.0  # Normalizza l'immagine
-        roi_resized = roi_resized.reshape(1, 300, 300, 3)
-
-        # Classifica la ROI
-        pred = classifier.predict(roi_resized)
-        print(pred)
-        class_id = pred.argmax(axis=1)[0]
-        class_name = classifier_class_names.get(class_id, 'Other')
-
         # Aggiungi informazioni sul frame
-        label = f'Class: {class_name}, Conf: {conf:.2f}'
+        label = f'Class: {cls}, Conf: {conf:.2f}'
         cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
         cv2.putText(frame, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
     
